ef6f7b9e34ebee916ce5733aa279b3769f01aba8
shashikant010 <shashikantyadav0503@gmail.com>
2024-04-20 15:47:09 +0530

    made some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import authService from './appwrite/auth'
+import { useDispatch } from 'react-redux'
+import { login,logout } from './store/authSlice'
+import { Footer, Header } from './components'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <h1>hello hii</h1>
-    </>
-  )
+  const [loading,setLoading]=useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login(userData))
+      } else{
+        dispatch(logout())
+      }
+    })
+    .catch((error)=>{
+      console.log("error in getting the data ",error)
+    })
+    .finally(()=>{
+      setLoading(false)
+    })
+  },[])
+
+
+  return !loading?(
+  <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+      <Header/>
+      <main>
+        {/* <Outlet/> */}
+      </main>
+      <Footer/>
+
+    </div>
+    </div>
+):null
+  
 }
 
 export default App
