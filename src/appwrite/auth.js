083714aa88950abfe2fb73c124de1c02df685bb9
shashikant010 <shashikantyadav0503@gmail.com>
2024-04-20 09:00:50 +0530

    wrote appwrite config services

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -4,6 +4,7 @@ import { Client,Account,ID } from "appwrite";
 
 export class AuthService{
     client=new Client()
+    account;
     constructor(){
         this.client
         .setEndpoint(conf.appwriteUrl)
