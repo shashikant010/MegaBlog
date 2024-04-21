134b3de2675f6bc918272644c242bfbbcc534191
shashikant010 <shashikantyadav0503@gmail.com>
2024-04-21 15:46:26 +0530

    did routing

--- src/components/Header/LogoutButton.jsx
+++ src/components/Header/LogoutButton.jsx
@@ -13,7 +13,9 @@ function LogoutButton() {
         })
     }
   return (
-    <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full' >
+    <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full' 
+    onClick={logoutHandler}
+    >
       Logout
     </button>
   )
--- src/components/index.js
+++ src/components/index.js
@@ -11,5 +11,6 @@ import Signup from "./Signup"
 import Login from "./Login"
 import PostForm from "./post-form/PostForm"
 import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
 
-export {Header,Footer,Container,Logo,LogoutButton,RTE,Button,Input,Select,Signup,Login,PostForm,PostCard}
+export {Header,Footer,Container,Logo,LogoutButton,RTE,Button,Input,Select,Signup,Login,PostForm,PostCard,AuthLayout}
