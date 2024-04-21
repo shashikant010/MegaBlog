40eb0ae11c5a8f353272c187c355047c41860e74
shashikant010 <shashikantyadav0503@gmail.com>
2024-04-21 12:37:34 +0530

    made all components

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,13 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutButton from "./Header/LogoutButton";
+import RTE from "./RTE";
+import Button from "./Button"
+import Input from "./Input";
+import Select from "./Select"
+import Signup from "./Signup"
+import Login from "./Login"
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard"
 
-export {Header,Footer,Container,Logo,LogoutButton}
+export {Header,Footer,Container,Logo,LogoutButton,RTE,Button,Input,Select,Signup,Login,PostForm,PostCard}
